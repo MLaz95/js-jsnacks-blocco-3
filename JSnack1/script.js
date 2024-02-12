2bd48a86e2104cbacb7f453654f3d95f48aa3fcb
marco <lazzari.marco.ml@gmail.com>
2024-02-12 13:08:14 +0100

    stampa risultato a schermo; JSnack1 completato

--- JSnack1/script.js
+++ JSnack1/script.js
@@ -2,13 +2,21 @@
 // Scrivi una funzione che accetti una stringa come argomento
 // e la restituisca girata (es. "Ciao" -> "oaiC")
 
+// memorizes needed html elements
+const inputElement = document.querySelector('#word');
+const buttonElement = document.querySelector('#button');
+const resultElement = document.querySelector('#result');
+
+// executes function on button click
+buttonElement.addEventListener('click', function(){
+    resultElement.innerHTML = invert(inputElement.value);
+})
+
+// --- FUNCTION ---
 function invert (word){
     let backWord = "";
     for(i = word.length - 1; i >= 0; i--){
         backWord += word[i];
-        
     }
     return backWord;
 }
-
-console.log(invert('test'))

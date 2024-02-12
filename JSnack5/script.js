181f45e8f052d9f8445bb3cc3b6188d3c02b3931
marco <lazzari.marco.ml@gmail.com>
2024-02-12 17:21:48 +0100

    Jsnack5 script divide un frase in singole parole correttamente

---
+++ JSnack5/script.js
@@ -0,0 +1,8 @@
+// Snack 5 (bonus):
+// Scrivi una funzione che accetti una stringa come parametro
+// e restituisca la stessa stringa ma con le iniziali delle parole tutte in maiuscolo
+
+const userInput = 'nel mezzo del cammin di nostra vita';
+const words = userInput.split(' ');
+
+console.log(words)
